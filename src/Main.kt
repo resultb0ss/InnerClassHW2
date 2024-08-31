c2e711ee2804691f58cdb46e70b34008af43dd1d
Admin <reteamboss@gmail.com>
2024-08-31 15:12:19 +0300

    Задание 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
 fun main() {
-    println("Hello World!")
+    var array = arrayOf("Иванов","Петров","Сидоров","Процветов","Протасов")
+
+
+    while (true) {
+        println("Введите символ для поиска, если хотите завершить нажмите 2")
+        var input = readln()
+        println("Возможно вы искали?")
+        for (elem in array) {
+            if (elem.contains(input, ignoreCase = true))
+                println(elem)
+        }
+        if (input == "2") {
+            println("Спасибо! Хорошего дня!")
+            break
+        }
+    }
 }
